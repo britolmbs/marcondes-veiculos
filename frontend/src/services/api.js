a29a18bc6298e9f65b7057253f9420da739c7053
Lucas Brito <britolmbs@hotmail.com>
2024-11-12 14:52:14 -0300

    criação da API.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,7 @@
+import axios from 'axios';
+
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5000/api';
+
+export const getMarcas = () => axios.get(`${API_BASE_URL}/marcas`);
+export const getModelos = (marcaId) => axios.get(`${API_BASE_URL}/modelos`, { params: { marcaId }});
+export const getVeiculo = (modeloId) => axios.get(`${API_BASE_URL}/veiculos`, { params: { modeloId }});
